3dcd9c7a6edbd647f30a5be85ad40f5e6efb40ef
Artur Baumeister <Artur.baumeister@gmail.com>
2022-10-01 08:42:46 +0200

    Done

--- app.js
+++ app.js
@@ -13,37 +13,50 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-hbs.registerPartials(__dirname + '/views/partials');
+//* Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 // ...
-
-// Add the route handlers here:
+const { randomSort } = require('./src/utilities');
+//* Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
-  console.log('/ ' + 'verb GET');
 });
 
-app.get('/beers', (req, res) => {
-  // const getBeers = () => {
+app.get('/beers/', (req, res) => {
+  const id = req.params.id;
+  console.log(id);
   punkAPI
     .getBeers()
-    .then(beersFromAPI =>
-      //console.log('beers from the database: ', beersFromAPI)
-      res.render('beers', { beersFromAPI })
-    )
+    .then(beersFromAPI => {
+      //beersFromAPI => console.log('beers from the database: ', beersFromAPI)
+      randomSort(beersFromAPI);
+      res.render('beers', { beersFromAPI });
+    })
     .catch(err => console.log(err));
+});
+
+app.get('/beers/:id', (req, res) => {
+  const id = req.params.id;
+  punkAPI
+    .getBeer(id)
+    .then(beersFromAPI => {
+      console.log(beersFromAPI);
+      res.render('randomBeer', { beersFromAPI: beersFromAPI });
+    })
+    .catch(error => console.log(error));
+  //res.send('Request failed!' + id);
 });
 
 app.get('/random-beer', (req, res) => {
   punkAPI
     .getRandom()
-    .then(responseFromAPI => {
+    .then(beersFromAPI => {
       // your magic happens here
 
-      console.log(responseFromAPI);
-      res.render('random-beer', { responseFromAPI }); // ## put this later in the .then
+      console.log(beersFromAPI);
+      res.render('randomBeer', { beersFromAPI: beersFromAPI });
     })
     .catch(error => console.log(error));
 });
---
+++ src/utilities.js
@@ -0,0 +1,5 @@
+const randomSort = arr => arr.sort(() => 0.5 - Math.random());
+//? Scroll to top mechanic?
+module.exports = {
+  randomSort
+};
